a22ddfb80110c66f96ff2de674755f17ccf109a7
Morgan Jackson <mbj4school@gmail.com>
2022-01-26 16:17:17 -0600

    Updated README + Some CSS tweaks

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # MicrAgg-WebApp
+
+First use the terminal to navigate into 'ma-app'
+
+From there, you will need to install react as first time setup, so type
+'npm install --save react'
+
+After that, you should be able to start the local dev server with 'npm start'
+To stop the server, press Ctrl+X
+
+Pushing code is just like normal Git, so we'll want to setup dev branches for the future,
+but to push to the online site, type 
+'npm run deploy'
--- ma-app/src/App.js
+++ ma-app/src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <iframe width="420" height="315"
+      <div className="App-content">
+        <div className="App-navBar">
+
+        </div>
+        <iframe className="App-video" title="Test Video"
           src="https://www.youtube.com/embed/fB8rHN4KvjQ?fs=0?playsinline=1&autoplay=1&modestbranding=1&rel=0">
         </iframe>
-      </header>
+        <div className="App-controls">
+
+        </div>
+      </div>
     </div>
   );
 }
