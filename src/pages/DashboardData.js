c6e9d7a732a94f9760e217c18e58f979aee5700c
Cameron Woodard <cawooda@siue.edu>
2022-02-10 18:18:12 -0600

    Initial DashboardData page and CSS file created

---
+++ src/pages/DashboardData.js
@@ -0,0 +1,22 @@
+import React from "react"
+import "./DashboardData.css"
+
+function DashboardMain() {
+    return (
+        <div className="DashboardMain-Main">
+            <div className="DashboardMain-Nav">
+                    <h4 className="Dashboard">Dashboard</h4>
+                    <h4>Data Options</h4>
+                <div className="DashboardData-Body">
+                    <button className="DashboardData-Button"></button>
+                    <button className="DashboardData-Button"></button>
+                    <button className="DashboardData-Button"></button>
+                </div>
+            </div>
+            <div className="DashboardMain-Body">
+            </div>
+        </div>
+    );
+}
+
+export default DashboardMain;
