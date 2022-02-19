44cfed4db43e911040c95681972654db4d4b82eb
Cameron Woodard <cawooda@siue.edu>
2022-02-19 15:15:54 -0600

    Changes to dashboard data and video retrieval

--- src/pages/DashboardData.js
+++ src/pages/DashboardData.js
@@ -1,17 +1,28 @@
 import React from "react"
 import "./DashboardData.css"
 import StandardPage from "../components/StandardPage";
+import VideoRetrieval from "../components/VideoRetrieval";
 
 function DashboardMain() {
     return (
         <StandardPage className="DashboardMain-Main">
+        <div>
+            <div className="DashboardData-eNav">
             <div className="DashboardMain-Nav">
-                    <h4 className="Dashboard">Dashboard</h4>
-                    <h4>Data Options</h4>
-                <div className="DashboardData-Body">
+                <h4 className="align">Dashboard</h4>
+                    {/* <h4 className="Dashboard">Dashboard</h4> */}
+                    <div className="DashboardData-Body-Nav">
+                        <h4>Data Options</h4>
+                    </div>
+                <div className="DashboardData-Body-Nav">
                     <button className="DashboardData-Button">Data View</button>
                     <button className="DashboardData-Button">Data Analysis</button>
                     <button className="DashboardData-Button">Video</button>
+                </div>
+            </div>
+                <div className="eNav">
+                    <VideoRetrieval videoPlay="1" data="true"/>
+                    <button>These are words</button>
                 </div>
             </div>
             <div className="DashboardMain-Body">
@@ -28,7 +39,8 @@ function DashboardMain() {
                     </table>
             </div>
             </div>
-        </StandardPage>
+            </div>
+         </StandardPage>
     );
 }
 
