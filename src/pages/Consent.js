f7e1c5df16692515f4889cc9efdf2ee632cde3b6
Morgan Jackson <mbj4school@gmail.com>
2022-02-22 15:23:13 -0600

    Formatting fixes for Consent Form and vid player

--- src/pages/Consent.js
+++ src/pages/Consent.js
@@ -1,39 +1,62 @@
-import React from "react"
+import React from "react";
 import { Link } from "react-router-dom";
-import "./Survey.css"
+import "./Survey.css";
 import StandardPage from "../components/StandardPage";
 
 export default function Consent() {
-        return (
-            <StandardPage className="Survey-Main">
-                <div className="Consent-Body">
-                <br />
-                    <div className="Consent-Content">
-                        <br />
-                        <p className="Consent-Text"> This is sample text that will fill in some things for the consent form. This
-                            will grow as it grows. It is here for testing and such but hey if it works it works. qqqqq
-                            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-                            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-                            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-                            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-                            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-                            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-                            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-                            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-                            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-                            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-                            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-                            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-                            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-                            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-                            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-                            qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
-                        </p>
-                    </div>
-                    <Link to="/VideoPlayer">
-                      <button className="Consent-Button">	&#8614;</button>
-                    </Link>
-                </div>
-            </StandardPage>
-        );   
+  return (
+    <StandardPage className="Survey-Main">
+      <div className="Consent-Body">
+        <br />
+        <div className="Consent-Content Consent-Text">
+          <h4>WELCOME AND NOTE ON INFORMED CONSENT</h4>
+          <p>
+            You are invited to participate in a survey to explore the impact of
+            the COVID-19 pandemic and digitalization on scientists. Your
+            participation will provide valuable insight into how the pandemic
+            and digitalization have impacted you and your scientific work. As
+            part of this survey, stressors and other psychological ailments you
+            experience in your work may be evoked. Answering this survey should
+            take about 15-20 minutes of your time.
+          </p>
+          <p>
+            The expected outcome of this survey is a peer-reviewed article in a
+            reputable scientific journal. You will have the option at the end of
+            the survey to go to another page and indicate if you would like to
+            be sent a copy of the published study.
+          </p>
+          <p>
+            Your information is anonymous (except for IP address and other
+            metadata) and confidential.
+          </p>
+          <p>
+            Participation is voluntary. If you choose to participate, you are
+            free to withdraw your consent and stop participating at any time.
+          </p>
+          <p>
+            The survey display is optimized for laptop or desktop computer
+            screens. However, you can also answer the survey on your cell phone
+            or tablet computer.
+          </p>
+          <p>
+            Upon completion, you can choose charities to receive a donation on
+            your behalf, and you may have the chance of winning a $100 gift
+            card.
+          </p>
+          <p>
+            If you have any questions, please contact Ass.-Prof. Dr. Magdalena
+            Bekk at bekk@wiso.uni-koeln.de or magdalena.bekk@uni-seeburg.at
+          </p>
+          <p>
+            YOU MAKE A DECISION WHETHER YOU WANT TO PARTICIPATE OR NOT. IF YOU
+            CLICK ON THE “CONTINUE” BUTTON, THIS MEANS THAT YOU HAVE DECIDED TO
+            PARTICIPATE. You may print a copy of this notice for your records.
+          </p>
+        </div>
+        <Link to="/VideoPlayer">
+          <button className="Consent-Button"> Continue</button>
+        </Link>
+      </div>
+    </StandardPage>
+  );
 }
--- src/pages/VideoPlayer.js
+++ src/pages/VideoPlayer.js
@@ -1,5 +1,6 @@
 import Vimeo from "@vimeo/player";
 import React from "react";
+import {Link} from "react-router-dom";
 import StandardPage from "../components/StandardPage";
 import VideoRetrieval from "../components/VideoRetrieval";
 import "./VideoPlayer.css";
@@ -56,6 +57,9 @@ function VideoPlayer() {
           <button class="VideoPlayer-button VideoPlayer-button1" onClick={() => UserResponse('Sensitive')}>Sensitive</button>
           <button class="VideoPlayer-button VideoPlayer-button2" onClick={() => UserResponse('Insensitive')}>Insensitive</button>
         </div>
+        <Link to="/Survey">
+          <button class="VideoPlayer-button" style={{height: 40}}>Continue</button>
+        </Link>
       </div>
     </StandardPage>
   );
