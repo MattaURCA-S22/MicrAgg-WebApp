4c876d370ca0df7565f9715e7df4bb0497813624
Morgan Jackson <mbj4school@gmail.com>
2022-03-14 18:29:37 -0500

    Began implementing pulling from firebase

--- src/components/OverallDataTable.js
+++ src/components/OverallDataTable.js
@@ -1,7 +1,40 @@
 import React from "react";
 import "./IndividualData.css";
+import { auth, db } from '../firebase';
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 export default function OverallDataTable(props) {
+  async function getUserData(){
+    const answersCol = collection(db, 'User-answers');
+    const answerSnapshot = await getDocs(answersCol);
+    const answerList = answerSnapshot.docs.map(doc => doc.data());
+    console.log(answerList)
+    return answerList;
+  }
+  
+
+  function returnRows(){
+    const promise = getUserData()
+    var rows = [];
+    promise.then((allData) => {
+      console.log(allData.length)
+      for (var i = 0; i < allData.length; i++) {
+        // note: we are adding a key prop here to allow react to uniquely identify each
+        // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
+        rows.push(
+          <tr>
+            <td>5</td>
+            <td>4</td>
+            <td>3</td>
+            <td>2</td>
+            <td>4</td>
+          </tr>
+        );
+      }
+      return rows;
+    });
+  }
+
   return (
     <div className="DashboardData-Body">
       <table className="DashboardData-Table">
@@ -23,21 +56,7 @@ export default function OverallDataTable(props) {
           <td>4</td>
         </tr>
 
-        <tr>
-          <td>igukbadkb</td>
-          <td>4</td>
-          <td>3</td>
-          <td>2</td>
-          <td>4</td>
-        </tr>
-
-        <tr>
-          <td>igukbadkb</td>
-          <td>4</td>
-          <td>3</td>
-          <td>2</td>
-          <td>4</td>
-        </tr>
+        {returnRows()}
       </table>
     </div>
   );
