045d2857a055efe947d1836ddb1b82ad99d1666d
Morgan Jackson <mbj4school@gmail.com>
2022-03-15 12:36:15 -0500

    Updates

---
+++ precache-manifest.07afacfce1109e8e8b5f36bf9b8272ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "db731601b419b05d871f",
+    "url": "/MicrAgg-WebApp/static/css/main.a146130e.chunk.css"
+  },
+  {
+    "revision": "db731601b419b05d871f",
+    "url": "/MicrAgg-WebApp/static/js/main.db731601.chunk.js"
+  },
+  {
+    "revision": "7f1662d5cca94d6900a5",
+    "url": "/MicrAgg-WebApp/static/js/1.7f1662d5.chunk.js"
+  },
+  {
+    "revision": "1499c2f16e4ce8299014",
+    "url": "/MicrAgg-WebApp/static/css/2.b563570b.chunk.css"
+  },
+  {
+    "revision": "1499c2f16e4ce8299014",
+    "url": "/MicrAgg-WebApp/static/js/2.1499c2f1.chunk.js"
+  },
+  {
+    "revision": "c4d155703af83eedb6bb",
+    "url": "/MicrAgg-WebApp/static/js/runtime~main.c4d15570.js"
+  },
+  {
+    "revision": "5ec99a46fa7948717072bd8f9e092fb4",
+    "url": "/MicrAgg-WebApp/index.html"
+  }
+];
