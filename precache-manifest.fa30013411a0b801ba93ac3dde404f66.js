46dbc2f3238c9b0135e06317189669e7dbab25e9
Morgan Jackson <morjack@siue.edu>
2022-09-02 19:10:19 -0500

    Updates

---
+++ precache-manifest.fa30013411a0b801ba93ac3dde404f66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce6e58eff3b6649babfc",
+    "url": "/static/css/main.9fae754e.chunk.css"
+  },
+  {
+    "revision": "ce6e58eff3b6649babfc",
+    "url": "/static/js/main.ce6e58ef.chunk.js"
+  },
+  {
+    "revision": "15c53034dcbdea72b049",
+    "url": "/static/js/1.15c53034.chunk.js"
+  },
+  {
+    "revision": "8ee6fe2d8ff610a24510",
+    "url": "/static/css/2.8c3aed92.chunk.css"
+  },
+  {
+    "revision": "8ee6fe2d8ff610a24510",
+    "url": "/static/js/2.8ee6fe2d.chunk.js"
+  },
+  {
+    "revision": "30bf50d6387f3a57c557",
+    "url": "/static/js/runtime~main.30bf50d6.js"
+  },
+  {
+    "revision": "f52b0fb0a770dd8f0d6113bb35f841d0",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MicrAgg-WebApp/precache-manifest.15b98198b1e22c54109205e987bc3c3a.js"
+  "/precache-manifest.fa30013411a0b801ba93ac3dde404f66.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/MicrAgg-WebApp/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
