39913d4de2290b2c69adc11097a66cd1e6aca316
Zach Sappington <zach.sappington@gmail.com>
2022-10-28 00:16:06 -0500

    Changed Q2,3,4 to have two parts on SurveyPage

--- src/pages/SurveyPage.js
+++ src/pages/SurveyPage.js
@@ -45,6 +45,7 @@ export default function SurveyPage() {
 
   var surveyJSON = {
     logoPosition: "right",
+    //questionTitlePattern: "numTitle", This line can be used to remove the required symbol from questions if need be
     pages: [
       {
         name: "page1",
@@ -69,128 +70,275 @@ export default function SurveyPage() {
               '<div className="surveyDiv">\n<strong>Please rate the therapist in the video based on how comfortable the therapist appears in discussing/addressing cultural dynamics or topics with the client.  </strong></div>',
           },
           {
-            type: "matrix",
-            name: "question2",
-            title:
-              "In your overall assessment, how comfortable did the therapist appear to you in the sessions (Parts 1 & 2) as they relate to dialogue around the client’s cultural identity or cultural issues? </br></br> The therapist appeared....",
-            isRequired: true,
-            columns: [
-              { value: "sd", text: "Strongly Disagree" },
-              { value: "md", text: "Disagree" },
-              { value: "n", text: "Neutral" },
-              { value: "ma", text: "Agree" },
-              { value: "sa", text: "Strongly Agree" },
-            ],
-            rows: [
-              { value: "comfortable", text: "1. Comfortable" },
-              { value: "awkward", text: "2. Awkward" },
-              { value: "tense", text: "3. Tense" },
-              { value: "nervous", text: "4. Nervous" },
-              { value: "confident", text: "5. Confident" },
-              { value: "uneasy", text: "6. Uneasy" },
-              { value: "relaxed", text: "7. Relaxed" },
-              { value: "calm", text: "8. Calm" },
-              { value: "edgy", text: "9. Edgy" },
-              { value: "genuine", text: "10. Genuine" },
-            ],
-          },
-          {
-            type: "matrix",
-            name: "question3",
-            title:
-              "In this next section, we would like for you to rate the therapist based on the following items. These items reflect the therapist’s cultural humility. As above, it is important to keep in mind the interactions specific to cultural issues in session (as opposed to general interactions). As a reminder, cultural humility is an other oriented mind-frame that is open to explore and understand the other’s worldview and a mind frame that does not assume superiority or expertise. However, this stance does not mean that the therapist is completely deferent to the client’s view, but rather invites the dialogue (two-person philosophy).<br/><br/>Overall, regarding the client’s cultural background, the therapist....",
-            isRequired: true,
-            columns: [
-              { value: "sd", text: "Strongly Disagree" },
-              { value: "md", text: "Disagree" },
-              { value: "n", text: "Neutral" },
-              { value: "ma", text: "Agree" },
-              { value: "sa", text: "Strongly Agree" },
-            ],
-            rows: [
-              { value: "_3a", text: "1. Is respectful." },
-              { value: "_3b", text: "2. Is open to explore." },
+            type: "panel",
+            name: "Q2_panel",
+            elements: [
               {
-                value: "_3c",
-                text: "3. Assumes she already knows a lot.",
-              },
-              { value: "_3d", text: "4. Is considerate." },
-              {
-                value: "_3e",
-                text: "5. Is genuinely interested in learning more.",
-              },
-              { value: "_3f", text: "6. Acts superior." },
-              {
-                value: "_3g",
-                text:
-                  "7. Is open to seeing things from the client's perspective.",
+                type: "matrix",
+                name: "question2",
+                title:
+                  "<center>PART 1</center>How comfortable did the therapist appear to you in PART 1 of the session as it relates to dialogue around the client’s cultural identity or cultural issues? </br></br> The therapist appeared....",
+                isRequired: true,
+                columns: [
+                  { value: "sd", text: "Strongly Disagree" },
+                  { value: "md", text: "Disagree" },
+                  { value: "n", text: "Neutral" },
+                  { value: "ma", text: "Agree" },
+                  { value: "sa", text: "Strongly Agree" },
+                ],
+                rows: [
+                  { value: "comfortable", text: "1. Comfortable" },
+                  { value: "awkward", text: "2. Awkward" },
+                  { value: "tense", text: "3. Tense" },
+                  { value: "nervous", text: "4. Nervous" },
+                  { value: "confident", text: "5. Confident" },
+                  { value: "uneasy", text: "6. Uneasy" },
+                  { value: "relaxed", text: "7. Relaxed" },
+                  { value: "calm", text: "8. Calm" },
+                  { value: "edgy", text: "9. Edgy" },
+                  { value: "genuine", text: "10. Genuine" },
+                ],
               },
               {
-                value: "_3h",
-                text: "8. Makes assumptions about the client.",
-              },
-              { value: "_3i", text: "9. Is open-minded." },
-              { value: "_3j", text: "10. Is a know-it-all." },
-              {
-                value: "_3k",
-                text:
-                  "11. The therapist seems to assume more understanding than the therapist actually has.",
-              },
-              {
-                value: "_3l",
-                text:
-                  "12. The therapist asks questions when the therapist is uncertain.",
+                type: "matrix",
+                name: "question2_part2",
+                title:
+                  "<center>PART 2</center>How comfortable did the therapist appear to you in PART 2 of the session as it relates to dialogue around the client’s cultural identity or cultural issues? </br></br> The therapist appeared....",
+                hideNumber: true,
+                isRequired: true,
+                columns: [
+                  { value: "sd", text: "Strongly Disagree" },
+                  { value: "md", text: "Disagree" },
+                  { value: "n", text: "Neutral" },
+                  { value: "ma", text: "Agree" },
+                  { value: "sa", text: "Strongly Agree" },
+                ],
+                rows: [
+                  { value: "comfortable", text: "1. Comfortable" },
+                  { value: "awkward", text: "2. Awkward" },
+                  { value: "tense", text: "3. Tense" },
+                  { value: "nervous", text: "4. Nervous" },
+                  { value: "confident", text: "5. Confident" },
+                  { value: "uneasy", text: "6. Uneasy" },
+                  { value: "relaxed", text: "7. Relaxed" },
+                  { value: "calm", text: "8. Calm" },
+                  { value: "edgy", text: "9. Edgy" },
+                  { value: "genuine", text: "10. Genuine" },
+                ],
               },
             ],
           },
           {
-            type: "matrix",
-            name: "question4",
-            title:
-              "Overall, please rate the degree to which the therapist in the video addressed (e.g., responded to or elicited) cultural issues within the sessions (Parts 1 & 2). In many cases, there are times where clients and therapists have the opportunity to discuss certain cultural issues more in depth (e.g., a client could mention in passing that their distress has shaken their faith in God, or that they feel empowered via a local social justice group around gay rights). These opportunities come and go. Sometimes they are important and other times, they are not. Please rate the following items regarding these cultural opportunities.",
-            isRequired: true,
-            columns: [
-              { value: "sd", text: "Strongly Disagree" },
-              { value: "md", text: "Disagree" },
-              { value: "n", text: "Neutral" },
-              { value: "ma", text: "Agree" },
-              { value: "sa", text: "Strongly Agree" },
-            ],
-            rows: [
+            type: "panel",
+            name: "Q3_panel",
+            elements: [
               {
-                value:
-                  "_4a",
-                text:
-                  "1. The therapist encouraged the client to discuss the client's cultural background more.",
+                type: "matrix",
+                name: "question3",
+                title:
+                  "In this next section, we would like for you to rate the therapist based on the following items. These items reflect the therapist’s cultural humility. As above, it is important to keep in mind the interactions specific to cultural issues in session (as opposed to general interactions). As a reminder, cultural humility is an other oriented mind-frame that is open to explore and understand the other’s worldview and a mind frame that does not assume superiority or expertise. However, this stance does not mean that the therapist is completely deferent to the client’s view, but rather invites the dialogue (two-person philosophy).<br><br><br><center>PART 1</center> In PART 1 of the session, regarding the client’s cultural background, the therapist....",
+                isRequired: true,
+                columns: [
+                  { value: "sd", text: "Strongly Disagree" },
+                  { value: "md", text: "Disagree" },
+                  { value: "n", text: "Neutral" },
+                  { value: "ma", text: "Agree" },
+                  { value: "sa", text: "Strongly Agree" },
+                ],
+                rows: [
+                  { value: "_3a", text: "1. Is respectful." },
+                  { value: "_3b", text: "2. Is open to explore." },
+                  {
+                    value: "_3c",
+                    text: "3. Assumes she already knows a lot.",
+                  },
+                  { value: "_3d", text: "4. Is considerate." },
+                  {
+                    value: "_3e",
+                    text: "5. Is genuinely interested in learning more.",
+                  },
+                  { value: "_3f", text: "6. Acts superior." },
+                  {
+                    value: "_3g",
+                    text:
+                      "7. Is open to seeing things from the client's perspective.",
+                  },
+                  {
+                    value: "_3h",
+                    text: "8. Makes assumptions about the client.",
+                  },
+                  { value: "_3i", text: "9. Is open-minded." },
+                  { value: "_3j", text: "10. Is a know-it-all." },
+                  {
+                    value: "_3k",
+                    text:
+                      "11. The therapist seems to assume more understanding than the therapist actually has.",
+                  },
+                  {
+                    value: "_3l",
+                    text:
+                      "12. The therapist asks questions when the therapist is uncertain.",
+                  },
+                ],
               },
               {
-                value:
-                  "_4b",
-                text:
-                  "2. The therapist discussed the client’s cultural background in a way that seemed to have worked.",
+                type: "matrix",
+                name: "question3_part2",
+                title:
+                  "<center>PART 2</center>In PART 2 of the session, regarding the client’s cultural background, the therapist....",
+                hideNumber: true,
+                isRequired: true,
+                columns: [
+                  { value: "sd", text: "Strongly Disagree" },
+                  { value: "md", text: "Disagree" },
+                  { value: "n", text: "Neutral" },
+                  { value: "ma", text: "Agree" },
+                  { value: "sa", text: "Strongly Agree" },
+                ],
+                rows: [
+                  { value: "_3a", text: "1. Is respectful." },
+                  { value: "_3b", text: "2. Is open to explore." },
+                  {
+                    value: "_3c",
+                    text: "3. Assumes she already knows a lot.",
+                  },
+                  { value: "_3d", text: "4. Is considerate." },
+                  {
+                    value: "_3e",
+                    text: "5. Is genuinely interested in learning more.",
+                  },
+                  { value: "_3f", text: "6. Acts superior." },
+                  {
+                    value: "_3g",
+                    text:
+                      "7. Is open to seeing things from the client's perspective.",
+                  },
+                  {
+                    value: "_3h",
+                    text: "8. Makes assumptions about the client.",
+                  },
+                  { value: "_3i", text: "9. Is open-minded." },
+                  { value: "_3j", text: "10. Is a know-it-all." },
+                  {
+                    value: "_3k",
+                    text:
+                      "11. The therapist seems to assume more understanding than the therapist actually has.",
+                  },
+                  {
+                    value: "_3l",
+                    text:
+                      "12. The therapist asks questions when the therapist is uncertain.",
+                  },
+                ],
+              },
+            ]
+          },
+          {
+            type: "panel",
+            name: "Q4_panel",
+            elements: [
+              {
+                type: "matrix",
+                name: "question4",
+                title:
+                  "Please rate the degree to which the therapist in the video addressed (e.g., responded to or elicited) cultural issues within the sessions. In many cases, there are times where clients and therapists have the opportunity to discuss certain cultural issues more in depth (e.g., a client could mention in passing that their distress has shaken their faith in God, or that they feel empowered via a local social justice group around gay rights). These opportunities come and go. Sometimes they are important and other times, they are not. Please rate the following items regarding these cultural opportunities.<br><br><br><center>PART 1</center> In PART 1 of the session....",
+                isRequired: true,
+                columns: [
+                  { value: "sd", text: "Strongly Disagree" },
+                  { value: "md", text: "Disagree" },
+                  { value: "n", text: "Neutral" },
+                  { value: "ma", text: "Agree" },
+                  { value: "sa", text: "Strongly Agree" },
+                ],
+                rows: [
+                  {
+                    value:
+                      "_4a",
+                    text:
+                      "1. The therapist encouraged the client to discuss the client's cultural background more.",
+                  },
+                  {
+                    value:
+                      "_4b",
+                    text:
+                      "2. The therapist discussed the client’s cultural background in a way that seemed to have worked.",
+                  },
+                  {
+                    value:
+                      "_4c",
+                    text:
+                      "3. The therapist avoided topics related to the client’s cultural background.",
+                  },
+                  {
+                    value: "_4d",
+                    text:
+                      "4. The therapist delved more deeply into the client’s cultural background.",
+                  },
+                  {
+                    value:
+                      "_4e",
+                    text:
+                      "5. There were many chances to have deeper discussions about the client’s cultural background that never happened.",
+                  },
+                  {
+                    value:
+                      "_4f",
+                    text:
+                      "6. The therapist missed opportunities to discuss the client’s cultural background.",
+                  },
+                ]
               },
               {
-                value:
-                  "_4c",
-                text:
-                  "3. The therapist avoided topics related to the client’s cultural background.",
-              },
-              {
-                value: "_4d",
-                text:
-                  "4. The therapist delved more deeply into the client’s cultural background.",
-              },
-              {
-                value:
-                  "_4e",
-                text:
-                  "5. There were many chances to have deeper discussions about the client’s cultural background that never happened.",
-              },
-              {
-                value:
-                  "_4f",
-                text:
-                  "6. The therapist missed opportunities to discuss the client’s cultural background.",
+                type: "matrix",
+                name: "question4_part2",
+                title:
+                  "<center>PART 2</center> In PART 2 of the session....",
+                hideNumber: true,
+                isRequired: true,
+                columns: [
+                  { value: "sd", text: "Strongly Disagree" },
+                  { value: "md", text: "Disagree" },
+                  { value: "n", text: "Neutral" },
+                  { value: "ma", text: "Agree" },
+                  { value: "sa", text: "Strongly Agree" },
+                ],
+                rows: [
+                  {
+                    value:
+                      "_4a",
+                    text:
+                      "1. The therapist encouraged the client to discuss the client's cultural background more.",
+                  },
+                  {
+                    value:
+                      "_4b",
+                    text:
+                      "2. The therapist discussed the client’s cultural background in a way that seemed to have worked.",
+                  },
+                  {
+                    value:
+                      "_4c",
+                    text:
+                      "3. The therapist avoided topics related to the client’s cultural background.",
+                  },
+                  {
+                    value: "_4d",
+                    text:
+                      "4. The therapist delved more deeply into the client’s cultural background.",
+                  },
+                  {
+                    value:
+                      "_4e",
+                    text:
+                      "5. There were many chances to have deeper discussions about the client’s cultural background that never happened.",
+                  },
+                  {
+                    value:
+                      "_4f",
+                    text:
+                      "6. The therapist missed opportunities to discuss the client’s cultural background.",
+                  },
+                ]
               },
             ],
           },
